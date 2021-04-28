9a642594db8bb090f5b5353300a948c8035183da
Zulhasnan277834 <80800877+Zulhasnan277834@users.noreply.github.com>
2021-04-28 09:22:54 +0800

    Delete Exercise4.2 directory

--- Exercise4.2/GreenApple.java
+++
@@ -1,9 +0,0 @@
-
-public class GreenApple extends Apple{//RedApple is a subclasses or extends from Apple class
-
-	public GreenApple (String name, String taste, double price) {
-		super(name,price);
-		this.taste = taste;
-		System.out.println("Green apple taste is " + this.taste);
-}
-}
